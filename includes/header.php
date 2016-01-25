c7372dbadb5ed38f6526266a75a084dcf9a93773
Charitha Sampath Gunawardana <charitha009@gmail.com>
2016-01-26 00:09:01 +0530

    Making main header 80% complete

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<div id="main-container">
+    <div id="main-header">
+        <img src="images/logo.png" alt="My Vacation Rental" class="logo"> 
+        <img src="images/logo-text.png" alt="My Vacation Rental" class="logo">
+        <h4>h1 lorem ipsum dolor, consectetur adipiscing elit. Pellentesque pulvinar purus in</h4>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Test</title>
+    <link rel="stylesheet" type="text/css" href="style/common-stylesheet.css">
+    <!--script type="text/javascript" src="$jsfile"></script-->
+</head>
+<body>
+    <?php 
+        include 'includes/header.php'; 
+    ?>
+    
+</body>
+</html>
