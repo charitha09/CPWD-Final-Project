328b8dbe9037503cdb51f37cd2b664ec4fcb9de1
Charitha Sampath Gunawardana <charitha009@gmail.com>
2016-01-26 13:25:00 +0530

    Main Header Complete

--- includes/header.php
+++ includes/header.php
@@ -3,5 +3,15 @@
         <img src="images/logo.png" alt="My Vacation Rental" class="logo"> 
         <img src="images/logo-text.png" alt="My Vacation Rental" class="logo">
         <h4>h1 lorem ipsum dolor, consectetur adipiscing elit. Pellentesque pulvinar purus in</h4>
+        <div id="header-navi-menu">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">ABOUT THE PROPERTY</a></li>
+                <li><a href="#">GALLERY</a></li>
+                <li><a href="#">LOCATION</a></li>
+                <li><a href="#">RATES & RESERVATIONS</a></li>
+                <li><a href="#">CONTACT US</a></li>
+            </ul>
+        </div>
     </div>
 </div>
--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 <head>
     <title>Test</title>
+    <meta charset="utf-8">
     <link rel="stylesheet" type="text/css" href="style/common-stylesheet.css">
-    <!--script type="text/javascript" src="$jsfile"></script-->
+    <script type="text/javascript" src="style/jquery-1.12.0.min.js"></script>
+    <script type="text/javascript" src="style/common-script.js"></script>
 </head>
 <body>
     <?php 
