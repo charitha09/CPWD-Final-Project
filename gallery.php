11a0bcda6e657cc32323cd9b381b2e212ab54c74
Charitha Sampath Gunawardana <charitha009@gmail.com>
2016-02-14 12:00:10 +0530

    Gallery Page 95% complete

--- gallery.php
+++ gallery.php
@@ -6,7 +6,24 @@
     include 'includes/cover.php';
     
     include 'includes/parth.php';
+?>
+<div class="margin-set">
+    <h2>GALLERY</h2>
+    <?php
+        $imageCount = 9;
+        $imageDiv = "";
+        for ( $i = 1; $i < ($imageCount+1); $i++ ){
+            $imageDiv .= <<<IDV
+            <div class="gallery-imgbox">
+                <img src="images/gallary/img-gallery-$i.jpg" alt="slide-$i" class="slide-show-images">
+            </div>        
+IDV;
+        }
+        echo $imageDiv;
+    ?>    
     
+</div>    
+<?php
     include 'includes/footer.php';
     
     include 'includes/endtag.php';
